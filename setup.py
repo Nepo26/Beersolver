92b66945fa01a50354e4391f4da665f926db899e
Henrique <nepo26.hn@protonmail.com>
2022-03-18 16:28:55 -0300

    doc: added readme to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
+with open('README.md') as f:
+    readme = f.read()
 
 
 setup(
     name='beersolver',
-    version='0.1.0',
+    version='0.1.1',
     author="Henrique Nepomuceno",
     author_email="nepo26.hn@protonmail.com",
     packages=find_packages(),
     py_modules=['exercises/ThirdChapter/commands'],
     description="A script for solving vector mechanics Ferdinand P. Beer proposed exercises.",
-    long_description=long_description,
+    long_description=readme,
+    long_description_content_type='text/markdown',
     url="https://github.com/Nepo26/Beersolver",
+    license="GNU GPLv3",
     install_requires=[
         'Click',
     ],
